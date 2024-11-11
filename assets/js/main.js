991387eaf77ffa1f5929c12e4616b78bda1ced0c
David Bonser <david@codapt.co.uk>
2024-11-11 10:22:20 +0000

    created base for component structure

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+/* Function that accepts file path and element id in order in inject into this index.html */
+function loadComponent(filePath, elementId) {
+  fetch(filePath)
+    .then((response) => response.text())
+    .then((text) => {
+      document.getElementById(elementId).innerHTML = text;
+    });
+}
+
+/* Function that will load list of components below */
+function loadAllComponents() {
+  loadComponent("components/header.html", "poodle-header");
+}
+
+/* Watches to see if the dom is loaded and runs loadAllComponents if it is */
+document.addEventListener("DOMContentLoaded", loadAllComponents);
